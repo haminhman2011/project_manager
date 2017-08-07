7d3e51db2c7ae19563fa8918ea73d5415d2fd2bd
haminhman2011 <haminhman2011@gmail.com>
2017-08-07 14:00:05 -0700

    create activity transfer

--- app/src/main/java/mh/manager/dialog/TransferActivity.java
+++ app/src/main/java/mh/manager/dialog/TransferActivity.java
@@ -1,15 +1,43 @@
 package mh.manager.dialog;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import mh.manager.R;
 
-public class TransferActivity extends AppCompatActivity {
+public class TransferActivity extends Activity {
+
+    public Button btnAssignAgent, btnCancel, btnClose;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         setContentView(R.layout.activity_transfer);
+
+        btnClose = (Button) findViewById(R.id.close_button);
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        btnCancel = (Button) findViewById(R.id.btnCancel);
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
